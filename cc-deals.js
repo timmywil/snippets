80e763f136410e40a77862fb61f97229d95e5956
Timmy Willison <timmywil@users.noreply.github.com>
2025-09-02 16:02:14 -0400

    feat(cc-deals): consolidate Amex and Chase scripts into cc-deals.js
    also add Wells Fargo support

---
+++ cc-deals.js
@@ -0,0 +1,37 @@
+// A bookmarklet for adding all AMEX, Chase, and Wells Fargo deals.
+// Minify with `npx uglify-js cc-deals.js` and use `pbcopy` or `clip` to copy to clipboard.
+// I'd normally use swc, but it requires installing deps first.
+javascript: (function () {
+    function randomDelay() {
+        return Math.random() * 1000 + 300
+    }
+    const btns = [
+        ...document.querySelectorAll('button[title="Add to Card"]'), // AMEX
+        ...document.querySelectorAll('[data-testid="deal-tile"]') // Wells Fargo
+    ]
+    function goBack() {
+        window.history.back()
+        setTimeout(add, randomDelay())
+    }
+    function closeModal() {
+        const modalClose = document.querySelector('[data-testid="modal-lightbox-close-icon"]')
+        if (modalClose) {
+            modalClose.click()
+            return setTimeout(add, randomDelay())
+        }
+        add()
+    }
+    function add() {
+        // Chase requires going to a new page
+        const chase = document.querySelector('[data-cy="commerce-tile-button"]')
+        if (chase) {
+            chase.click()
+            return setTimeout(goBack, randomDelay())
+        }
+        const btn = btns.pop()
+        if (!btn) return console.log('All offers added!')
+        btn.click()
+        setTimeout(closeModal, randomDelay())
+    }
+    add()
+})()
